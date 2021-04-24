d49745b84a622f6703bbdae9d7640968776c0919
Marco Kang <kmnkit@gmail.com>
2021-04-25 00:20:03 +0900

    Block Hash 구하기

---
+++ README.md
@@ -0,0 +1,50 @@
+# 블록체인 공부
+
+## 블록체인 작성
+- [x] 블록 작성
+- [x] 블록체인 구조체 작성
+- [x] 블록의 해쉬 구하기
+- [ ] 트랜잭션 추가
+- [ ] Proof of Work, 콘센서스 알고리즘, 해(Nonce)에 대해
+- [ ] 해(Nonce) 구하기
+- [ ] Mining
+- [ ] Transaction 합계 구하기
+
+## 월렛 작성
+
+- [ ] 비밀키와 공개키, ECDSA
+- [ ] 월렛 작성
+- [ ] 블록체인 어드레스 작성방법
+- [ ] 블록체인 어드레스 작성
+- [ ] Transaction 서명
+- [ ] Transaction 검증
+
+## 블록체인 서버 API
+
+- [ ] 웹서버 구동
+- [ ] 블록체인을 취득하는 API 작성
+- [ ] 월렛의 UI서버 구동
+- [ ] 월렛의 UI 작성
+- [ ] 월렛 작성
+- [ ] UI에서 Transaction 송신
+- [ ] Json Decode
+- [ ] 문자열에서 ECDSA관련 데이터를 구조체로 변환하기
+- [ ] HTTP Client로 데이터 송신
+- [ ] Transaction API 작성
+- [ ] Mining API 작성
+- [ ] 자동 Mining 실행
+- [ ] 가상화폐의 합계를 반환하는 Blockchain 서버 API 작성
+- [ ] 가상화폐의 합계를 반환하는 월렛 서버의 API 작성
+- [ ] 가상화폐의 합계를 표시하는 UI 작성
+
+## 블록체인 네트워크 구축
+
+- [ ] 다른 Blockchain Node를 탐색하기
+- [ ] Blockchain Node의 자동등록
+- [ ] Transaction 동기화
+- [ ] 콘센서스란
+- [ ] Blockchain 검증
+- [ ] 가장 긴 것을 채용하는 Resolve Conflict
+- [ ] 콘센서스 API 작성
+- [ ] 51% 어택에 대하여
+- [ ] 최종 데모
--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -10,34 +12,47 @@ import (
 // Block 블록 기본 구조체
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	timestamp    int64
 	transactions []string
 }
 
 // NewBlock 새 블록 생성
-func NewBlock(nonce int, previousHash string) *Block {
-
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	// Block의 address를 반환함
-	// 방법 1
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
 	b.nonce = nonce
 	b.previousHash = previousHash
 	return b
+}
 
-	// 방법 2
-	// return &Block{
-	// 	timestamp: time.Now().UnixNano(), // UnixNano는 int64타입의 값을 반환함.
-	// }
+// MarshalJSON JSON 형태 지정
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Timestamp    int64    `json:"timestamp"`
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+	})
 }
 
 // Print 블록의 정보를 출력하는 메소드
 func (b *Block) Print() {
 	fmt.Printf("timestamp      %d\n", b.timestamp)
 	fmt.Printf("nonce          %d\n", b.nonce)
-	fmt.Printf("previousHash   %s\n", b.previousHash)
+	fmt.Printf("previousHash   %x\n", b.previousHash)
 	fmt.Printf("transactions   %s\n", b.transactions)
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
 }
 
 // Blockchain 블록체인 구조체
@@ -48,21 +63,23 @@ type Blockchain struct {
 
 // NewBlockChain 새 블록체인 작성
 func NewBlockChain() *Blockchain {
-	/*
-		새 블록체인을 작성한다.
-		처음 블록에는 PreviousHash가 없으므로 InitHash를 작성하여 넣어준다.
-		그 블록체인을 넘겨준다.
-	*/
+	// 새 블록체인을 작성한여 넘겨준다.
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.CreateBlock(0, "InitHash")
+	bc.CreateBlock(0, b.Hash())
 	return bc
 }
 
 // CreateBlock 블록을 하나 추가하여 블록체인에 그 블록을 추가함
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash) // 블록 작성
 	bc.chain = append(bc.chain, b)     // bc의 체인에 위의 블록을 추가
 	return b
+}
+
+// LastBlock 블록체인의 마지막 블록을 구함
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
 }
 
 // Print 블록 체인 안의 체인 안의 모든 블록을 출력함
@@ -82,9 +99,13 @@ func init() {
 func main() {
 	blockChain := NewBlockChain()
 	blockChain.Print()
-	blockChain.CreateBlock(5, "hash 1")
+
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(5, previousHash)
 	blockChain.Print()
-	blockChain.CreateBlock(2, "hash 2")
+
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash)
 	blockChain.Print()
 	return
 }
