3c8d3b73a7c38aa95c8f8d22460576dbbd0344e1
Marco Kang <kmnkit@gmail.com>
2021-04-26 00:03:07 +0900

    Json Decode

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
 	"log"
@@ -57,7 +59,24 @@ func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
 func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
-		io.WriteString(w, string(utils.JsonStatus("success")))
+		decoder := json.NewDecoder(req.Body)
+		var t wallet.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		if !t.Validate() {
+			log.Println("ERROR: missing field(s")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		fmt.Println(*t.SenderPublicKey)
+		fmt.Println(*t.SenderBlockchainAddress)
+		fmt.Println(*t.SenderPrivateKey)
+		fmt.Println(*t.RecipientBlockchainAddress)
+		fmt.Println(*t.Value)
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		log.Println("HERROR: Invalid HTTP Method")
